7c70788e589b316557ce2db47b173b3c8f577ca3
Mark Derricutt <mark@talios.com>
2012-08-11 20:33:11 +1200

    Initial commit for frege testing project.

---
+++ src/test/java/com/talios/frege/AppTest.java
@@ -0,0 +1,36 @@
+package com.talios.frege;
+
+import com.theoryinpractise.halbuilder.RepresentationFactory;
+import com.theoryinpractise.halbuilder.spi.Representation;
+import frege.prelude.PreludeBase;
+import frege.rt.Box;
+import org.testng.annotations.Test;
+
+import static com.talios.frege.Greeting.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    public void testDataType() {
+
+        TGreeting newGreeting  = TGreeting.mk(Box.mk("Mark"), TSalutation.mk(Box.mk("Hello there"), Box.mk("Chow!")));
+
+        System.out.println(MyFrege.testGreeting._e().toString());
+
+        assertThat(TGreeting.name(newGreeting)).isEqualTo("Mark");
+        assertThat(TSalutation.welcome(TGreeting.salutation(newGreeting))).isEqualTo("Hello there");
+
+        String greet = MyFrege.makeGreeting(newGreeting);
+        String farewell = MyFrege.makeFarewell(newGreeting);
+
+        System.out.println(farewell);
+
+        PreludeBase.TST.performUnsafe(MyFrege.greet(newGreeting))._e();
+
+        assertThat(greet).isEqualTo("Hello there Mark, how are you?");
+
+        com.talios.frege.Testing.main(new String[] {""});
+    }
+
+}
